33812c59d7e0b7df6653795f8a64b40c2b3cf67b
UdiptaMitra <youdipta09@gmail.com>
2025-10-16 12:34:53 +0530

    Update Iterative10_(automorphic).c

--- Iterative10_(automorphic).c
+++ Iterative10_(automorphic).c
@@ -1,16 +1,23 @@
 /*WAPC to input a positive integer. Check whether it is Automorphic or not. 
 A number whose square ends with the number itself is an Automorphic number. 
-Example: 25² = 625 (ends with 25)*/
+Example: 25Â² = 625 (ends with 25)*/
 
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    unsigned int num;
-    int square,lastDigits;
+    unsigned int num,copy;
+    int square,lastDigits,len=0;
     printf("\nInput a positive number: ");
     scanf("%u",&num);
     square=num*num;
-    lastDigits=square%100;
+    copy=num;
+    while(copy>0)
+        {
+            len++;
+            copy/=10;
+        }
+    lastDigits=square%(pow(10,len));
     if(lastDigits==num)
     {
         printf("\nthe number %u is an automorphic number",num);
@@ -21,3 +28,4 @@ int main()
     }
     return 0;
 }
+
